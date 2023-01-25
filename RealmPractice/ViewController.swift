8e1f04a757f26a38470865b2e81c5bce54385e74
Hyunndy <yoosa3004@deali.net>
2023-01-25 17:23:27 +0900

    Initial Commit

---
+++ RealmPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmPractice
+//
+//  Created by 유현지 on 2023/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
